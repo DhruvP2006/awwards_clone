f6186dd88847f0eafc243b08893e96a0b73c203e
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2025-07-27 23:59:41 +0530

    fix

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -28,8 +28,8 @@ export const BentoTilt = ({ children, className = "" }) => {
 
   return (
     <div
-      className={className}
       ref={itemRef}
+      className={className}
       onMouseMove={handleMouseMove}
       onMouseLeave={handleMouseLeave}
       style={{ transform: transformStyle }}
@@ -53,7 +53,7 @@ export const BentoCard = ({ src, title, description }) => {
         <div>
           <h1 className="bento-title special-font">{title}</h1>
           {description && (
-            <p className="mt-3 max-w-64 text-xs">{description}</p>
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
           )}
         </div>
       </div>
@@ -64,7 +64,7 @@ export const BentoCard = ({ src, title, description }) => {
 const Features = () => (
   <section className="bg-black pb-52">
     <div className="container mx-auto px-3 md:px-10">
-      <div className="px-5 py-32 ">
+      <div className="px-5 py-32">
         <p className="font-circular-web text-lg text-blue-50">
           Into the Metagame Layer
         </p>
@@ -79,13 +79,13 @@ const Features = () => (
           src="videos/feature-1.mp4"
           title={
             <>
-              Radia<b>n</b>t
+              radia<b>n</b>t
             </>
           }
           description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
         />
       </BentoTilt>
-      <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+      <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7 w-full">
         <BentoTilt className="bento-tilt_1  row-span-1 md:col-span-1 md:row-span-2">
           <BentoCard
             src="videos/feature-2.mp4"
@@ -123,18 +123,18 @@ const Features = () => (
           <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
             <h1 className="bento-title special-font max-w-64 text-black">
               M<b>o</b>re co<b>m</b>ing s<b>o</b>on!
-              <TiLocationArrow className="m-5 scale-[5] self-end" />
             </h1>
+            <TiLocationArrow className="m-5 scale-[5] self-end" />
           </div>
         </BentoTilt>
         <BentoTilt className="bento-tilt_2 ">
           <video
-            src="video/features-5.mp4"
+            src="videos/features-5.mp4"
             loop
             muted
             autoPlay
             className="size-full object-cover object-center"
-          ></video>
+          />
         </BentoTilt>
       </div>
     </div>
